507f0b49422110841787cab518c11c52872ac20e
Присяжнюк Вячеслав <prslava@mail.ru>
2021-07-24 21:18:46 +0300

    попытка один

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,10 +1,24 @@
 package jm.task.core.jdbc;
 
+import com.mysql.cj.Session;
 import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.SessionFactory;
+
 public class Main {
     public static void main(String[] args) {
+        UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
         UserDao userDao = new UserDaoJDBCImpl();
+        userDaoHibernate.createUsersTable();
+        userDaoHibernate.saveUser("Bog", "Bogovih", (byte) 45);
+        userDaoHibernate.saveUser("gaf", "strak", (byte) 33);
+        userDaoHibernate.saveUser("pip", "poc", (byte) 23);
+        userDaoHibernate.saveUser("nub", "ger", (byte) 76);
+        userDaoHibernate.getAllUsers();
+        userDaoHibernate.cleanUsersTable();
+        userDaoHibernate.dropUsersTable();
         userDao.createUsersTable();
         userDao.saveUser("Ivanic", "Ivanov", (byte) 5);
         userDao.saveUser("Slava", "Prisyazhnuk", (byte) 33);
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,88 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
-
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
-
     }
-
-
     @Override
     public void createUsersTable() {
-
+        Session session = Util.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        session.createSQLQuery("CREATE TABLE IF NOT EXISTS users (Id INT PRIMARY KEY AUTO_INCREMENT, Name VARCHAR(20), lastName VARCHAR(20), age INT)")
+                .addEntity(User.class)
+                .executeUpdate();
+        tx.commit();
+        session.close();
     }
 
     @Override
     public void dropUsersTable() {
+        Session session = Util.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        session.createSQLQuery("DROP TABLE IF EXISTS users")
+                .executeUpdate();
+        tx.commit();
+        session.close();
 
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try {
+            SessionFactory fg = Util.getSessionFactory();
+            Session session = fg.openSession();
+            System.out.println(fg.isOpen());
+            System.out.println("Добавление записи в таблицу БД");
+            Transaction tx = session.beginTransaction();
+                User person = new User(name, lastName, age);
+                session.save(person);
+            tx.commit();
+            System.out.println("\tЗаписи добавлены");
+            session.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
-
     @Override
     public void removeUserById(long id) {
-
+        Session session = Util.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        User user = (User) session.get(User.class, id);
+        session.delete(user);
+        tx.commit();
+        session.close();
     }
-
     @Override
     public List<User> getAllUsers() {
-        return null;
-    }
-
+        Session session = Util.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        List <User> list;
+        list = session.createCriteria(User.class).list();
+        for (User item: list) {
+            System.out.println(item);
+        }
+        tx.commit();
+        session.close();
+            return list;
+        }
     @Override
     public void cleanUsersTable() {
-
+        Session session = Util.getSessionFactory().openSession();
+        Transaction tx = session.beginTransaction();
+        final List<User> list = session.createCriteria(User.class).list();
+        if(list.size() > 0) {
+            for (User obj : list) {
+                session.delete(obj);
+            }
+        }
+            tx.commit();
+            session.close();
+        }
     }
-}
+
