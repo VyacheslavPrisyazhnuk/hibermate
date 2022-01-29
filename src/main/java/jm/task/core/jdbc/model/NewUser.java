d041893d4cab5da5e0edd06c140821338c4657b1
Присяжнюк Вячеслав <prslava@mail.ru>
2022-01-29 20:56:29 +0300

    попытка один3

--- src/main/java/jm/task/core/jdbc/model/NewUser.java
+++ src/main/java/jm/task/core/jdbc/model/NewUser.java
@@ -3,5 +3,6 @@ package jm.task.core.jdbc.model;
 public class NewUser {
     public static void main(String[] args) {
         System.out.println("коммит1");
+        System.out.println("branch1");
     }
 }
