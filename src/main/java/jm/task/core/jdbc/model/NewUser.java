59f3f9eaaa753b056ef2bc8d1d41165dd21a19a7
Присяжнюк Вячеслав <prslava@mail.ru>
2022-01-29 21:04:34 +0300

    sout4

--- src/main/java/jm/task/core/jdbc/model/NewUser.java
+++ src/main/java/jm/task/core/jdbc/model/NewUser.java
@@ -1,7 +1,11 @@
+
 package jm.task.core.jdbc.model;
 
 public class NewUser {
     public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
         System.out.println("коммит1");
         System.out.println("branch1");
         System.out.println("branch1 commit");
