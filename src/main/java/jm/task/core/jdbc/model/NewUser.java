b7cf0322b2409f2043edfc6824041a711b24af50
Присяжнюк Вячеслав <prslava@mail.ru>
2022-01-29 20:57:28 +0300

    попытка один4

--- src/main/java/jm/task/core/jdbc/model/NewUser.java
+++ src/main/java/jm/task/core/jdbc/model/NewUser.java
@@ -4,5 +4,6 @@ public class NewUser {
     public static void main(String[] args) {
         System.out.println("коммит1");
         System.out.println("branch1");
+        System.out.println("branch1 commit");
     }
 }
